9562c20ec049f49b8133e86c9c4a5845d14c169f
ZhukovaYu <zhukova.julia@gmail.com>
2023-01-05 20:25:24 +0100

    goit-js-hw-08

---
+++ src/js/03-feedback.js
@@ -0,0 +1,6 @@
+import throttle from 'lodash.throttle';
+
+// const formEl = document.querySelector('.feedback-form');
+ 
+// formEl.addEventListener('input', throttle(onInput, 500));
+// formEl.addEventListener('submit', onSubmit);
