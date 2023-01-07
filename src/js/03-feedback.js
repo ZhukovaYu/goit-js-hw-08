429f56b07bd98b138eb3e6432c29a37cecf0469c
ZhukovaYu <113676493+ZhukovaYu@users.noreply.github.com>
2023-01-07 11:34:23 +0000

    Deploying to gh-pages from @ ZhukovaYu/goit-js-hw-08@05f776277f4506cc8db0db13cc076f5532833400 🚀

--- src/js/03-feedback.js
+++
@@ -1,6 +0,0 @@
-import throttle from 'lodash.throttle';
-
-// const formEl = document.querySelector('.feedback-form');
- 
-// formEl.addEventListener('input', throttle(onInput, 500));
-// formEl.addEventListener('submit', onSubmit);
