372480687c79ad71bacad4b263de2897c5c05957
ZhukovaYu <zhukova.julia@gmail.com>
2023-01-07 12:28:35 +0100

    goit-js-hw-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,34 @@
 import throttle from 'lodash.throttle';
+// import localStorageApi from './local-storage-api';
 
-// const formEl = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+};
+
+fillInInfo();
+
+refs.textarea.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormInput(evt) { 
+    const input = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, input);
+}
+
+function onFormSubmit(evt) { 
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function fillInInfo() { 
+    const savedInfo = localStorage.getItem(STORAGE_KEY);
+    if (STORAGE_KEY) {
+        console.log(savedInfo);
+        refs.textarea.value = savedInfo;
+    }
+}
  
-// formEl.addEventListener('input', throttle(onInput, 500));
-// formEl.addEventListener('submit', onSubmit);
