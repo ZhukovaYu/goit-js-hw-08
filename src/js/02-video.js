1bb63584eaf0655983ab795607260d54c3b30130
ZhukovaYu <zhukova.julia@gmail.com>
2023-01-08 12:37:20 +0100

    goit-js-hw-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,4 +10,7 @@ function onPlay({ seconds }) {
 localStorage.setItem('videoplayer-current-time', seconds);
 }
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime!== null) {
+    player.setCurrentTime(savedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,33 +1,11 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
 
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('.feedback-form textarea'),
-};
+feedbackForm.addEventListener('input', evt => {
 
-fillInInfo();
+    console.log(evt.target.name);
+    console.log(evt.target.value);
 
-refs.textarea.addEventListener('input', throttle(onFormInput, 500));
-refs.form.addEventListener('submit', onFormSubmit);
-
-function onFormInput(evt) { 
-    const input = evt.target.value;
-    localStorage.setItem(STORAGE_KEY, input);
-}
-
-function onFormSubmit(evt) { 
-    evt.preventDefault();
-    evt.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
-}
-
-function fillInInfo() { 
-    const savedInfo = localStorage.getItem(STORAGE_KEY);
-    if (STORAGE_KEY) {
-        console.log(savedInfo);
-        refs.textarea.value = savedInfo;
-    }
-}
- 
+});
