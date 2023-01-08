428ce542642f30401c3ffd7bc57fcdc28378abcb
ZhukovaYu <zhukova.julia@gmail.com>
2023-01-08 13:08:29 +0100

    goit-js-hw-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,32 @@
 import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = 'feedback-form-state';
 const feedbackForm = document.querySelector('.feedback-form');
 
-feedbackForm.addEventListener('input', evt => {
+initForm();
 
-    console.log(evt.target.name);
-    console.log(evt.target.value);
+const onFormItemChange =  evt => {
+    let persistedForm = localStorage.getItem('feedback-form-state');
+    persistedForm = persistedForm ? JSON.parse(persistedForm) : {};
+    persistedForm[evt.target.name] = evt.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(persistedForm));
+};
 
-});
+function initForm() {
+    let persistedForm = localStorage.getItem('feedback-form-state')
+    if(persistedForm) {
+        persistedForm = JSON.parse(persistedForm);
+        Object.entries(persistedForm).forEach(([name,value]) => {  
+        feedbackForm.elements[name].value = value;
+        });
+    }
+}; 
+
+const onFormSubmit = event => {
+    event.preventDefault();
+    console.log();
+    feedbackForm.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+
+feedbackForm.addEventListener('input', throttle(onFormItemChange, 500));
+feedbackForm.addEventListener('submit',onFormSubmit);
